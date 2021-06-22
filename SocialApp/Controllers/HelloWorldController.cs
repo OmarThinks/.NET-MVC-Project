a738b70e7d5a68641cb48a74c059c861896c4653
Omar Magdy <contact.parmag@gmail.com>
2021-06-22 17:19:59 +0200

    HelloWorldController:add:more endpoints

--- SocialApp/Controllers/HelloWorldController.cs
+++ SocialApp/Controllers/HelloWorldController.cs
@@ -20,5 +20,14 @@ namespace MvcMovie.Controllers
         {
             return "This is the Welcome action method...";
         }
+
+        public string Welcome2(string name, int numTimes = 1)
+        {
+            return HtmlEncoder.Default.Encode($"Hello {name}, NumTimes is: {numTimes}");
+        }
+        public string Welcome3(string name, int ID = 1)
+        {
+            return HtmlEncoder.Default.Encode($"Hello {name}, id is: {ID}");
+        }
     }
 }
