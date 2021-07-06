2f0df0f19c86cc8ec4bb174052eaa659fbdf0795
Omar Magdy <contact.parmag@gmail.com>
2021-07-06 23:04:22 +0200

    Created commnet and Post models

---
+++ SocialApp/Models/Comment.cs
@@ -0,0 +1,9 @@
+using System;
+
+namespace SocialApp.Models
+{
+    public class Comment
+    {
+        public string Content { get; set; }
+    }
+}
---
+++ SocialApp/Models/Post.cs
@@ -0,0 +1,9 @@
+using System;
+
+namespace SocialApp.Models
+{
+    public class Post
+    {
+        public string Content { get; set; }
+    }
+}
