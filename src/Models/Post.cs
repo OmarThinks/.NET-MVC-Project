530de9da5a11769978e6f16412596021faeced75
Omar Magdy <contact.parmag@gmail.com>
2021-08-29 14:16:21 +0200

    Post.cs:created

---
+++ src/Models/Post.cs
@@ -0,0 +1,11 @@
+using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace SocialApp.Models
+{
+    public class Post
+    {
+        public int Id { get; set; }
+        //public string Con { get; set; }
+    }
+}
