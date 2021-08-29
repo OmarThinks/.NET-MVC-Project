e4b4fa0bc219461b9d4300c369d7dcb3b56731ce
Omar Magdy <contact.parmag@gmail.com>
2021-08-29 16:13:14 +0200

    Added Content field to the Post Model

--- src/Models/Post.cs
+++ src/Models/Post.cs
@@ -6,16 +6,22 @@ namespace SocialApp.Models
     public class Post
     {
         public int Id { get; set; }
-        //public string Con { get; set; }
+        public string Content { get; set; }
     }
 }
 
 
 /*
 
- dotnet new webapp --output "src" --name "SocialApp" --auth Individual
+dotnet new webapp --output "src" --name "SocialApp" --auth Individual
+
 
 
 dotnet aspnet-codegenerator controller --controllerName postsController -outDir Controllers --model Post --dataContext SocialApp.Data.ApplicationDbContext -udl -scripts
+
+
+dotnet ef migrations add InitialCreate
+dotnet ef migrations list
+dotnet ef database update
 
 */
