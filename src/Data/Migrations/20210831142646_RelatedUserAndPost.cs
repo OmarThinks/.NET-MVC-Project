30be7d75bda90946ad343e1a89135f82421089f2
Omar Magdy <contact.parmag@gmail.com>
2021-08-31 16:32:03 +0200

    Migrated Post relationship with User

---
+++ src/Data/Migrations/20210831142646_RelatedUserAndPost.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Application.Data.Migrations
+{
+    public partial class RelatedUserAndPost : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "AuthorId",
+                table: "Post",
+                type: "INTEGER",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<string>(
+                name: "AuthorId1",
+                table: "Post",
+                type: "TEXT",
+                nullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Post_AuthorId1",
+                table: "Post",
+                column: "AuthorId1");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Post_AspNetUsers_AuthorId1",
+                table: "Post",
+                column: "AuthorId1",
+                principalTable: "AspNetUsers",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Post_AspNetUsers_AuthorId1",
+                table: "Post");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Post_AuthorId1",
+                table: "Post");
+
+            migrationBuilder.DropColumn(
+                name: "AuthorId",
+                table: "Post");
+
+            migrationBuilder.DropColumn(
+                name: "AuthorId1",
+                table: "Post");
+        }
+    }
+}
