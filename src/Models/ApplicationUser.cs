7aff682d9c7039faf99ea3979d203027477a1c3b
Omar Magdy <contact.parmag@gmail.com>
2021-08-31 12:06:12 +0200

    Application user model filled

---
+++ src/Models/ApplicationUser.cs
@@ -0,0 +1,12 @@
+using System;
+using Microsoft.AspNetCore.Identity;
+using SocialApp.Models;
+using System.Collections.Generic;
+
+namespace SocialApp.Models
+{
+    public class ApplicationUser : IdentityUser
+    {
+        //public List<Post> Posts { get; set; }       
+    }
+}
