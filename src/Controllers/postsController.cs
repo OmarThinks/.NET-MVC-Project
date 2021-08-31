0d53b8acd8ed907d21e0900f09140187f034b589
Omar Magdy <contact.parmag@gmail.com>
2021-08-31 12:43:50 +0200

    PostsController:add:Authorize

--- src/Controllers/postsController.cs
+++ src/Controllers/postsController.cs
@@ -7,6 +7,7 @@ using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
 using Application.Data;
 using Application.Models;
+using Microsoft.AspNetCore.Authorization;
 
 namespace Application.Controllers
 {
@@ -26,6 +27,7 @@ namespace Application.Controllers
         }
 
         // GET: posts/Details/5
+
         public async Task<IActionResult> Details(int? id)
         {
             if (id == null)
@@ -44,6 +46,8 @@ namespace Application.Controllers
         }
 
         // GET: posts/Create
+        [Authorize]
+
         public IActionResult Create()
         {
             return View();
@@ -54,6 +58,8 @@ namespace Application.Controllers
         // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
+        [Authorize]
+
         public async Task<IActionResult> Create([Bind("PostId,Content")] Post post)
         {
             if (ModelState.IsValid)
@@ -66,6 +72,8 @@ namespace Application.Controllers
         }
 
         // GET: posts/Edit/5
+        [Authorize]
+
         public async Task<IActionResult> Edit(int? id)
         {
             if (id == null)
@@ -86,6 +94,8 @@ namespace Application.Controllers
         // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
+        [Authorize]
+
         public async Task<IActionResult> Edit(int id, [Bind("PostId,Content")] Post post)
         {
             if (id != post.PostId)
@@ -117,6 +127,8 @@ namespace Application.Controllers
         }
 
         // GET: posts/Delete/5
+        [Authorize]
+
         public async Task<IActionResult> Delete(int? id)
         {
             if (id == null)
@@ -137,6 +149,8 @@ namespace Application.Controllers
         // POST: posts/Delete/5
         [HttpPost, ActionName("Delete")]
         [ValidateAntiForgeryToken]
+        [Authorize]
+
         public async Task<IActionResult> DeleteConfirmed(int id)
         {
             var post = await _context.Post.FindAsync(id);
