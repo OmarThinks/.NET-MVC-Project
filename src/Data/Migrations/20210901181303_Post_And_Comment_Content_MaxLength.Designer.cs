1e076d210f983e358dd809922dfb585ebfccd0ed
Omar Magdy <contact.parmag@gmail.com>
2021-09-01 20:14:44 +0200

    Migrated Post and Comment Max Length

--- src/Data/Migrations/20210901181303_Post_And_Comment_Content_MaxLength.Designer.cs
+++ src/Data/Migrations/20210901181303_Post_And_Comment_Content_MaxLength.Designer.cs
@@ -3,14 +3,16 @@ using System;
 using Application.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace Application.Data.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20210901181303_Post_And_Comment_Content_MaxLength")]
+    partial class Post_And_Comment_Content_MaxLength
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
